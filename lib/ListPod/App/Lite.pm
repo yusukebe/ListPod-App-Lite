e312a1b39df89e987a078a4b9d80fe7f2b7a90b7
Yusuke Wada <yusuke@kamawada.com>
2010-03-14 23:32:57 +0900

    initial commit

---
+++ lib/ListPod/App/Lite.pm
@@ -0,0 +1,32 @@
+package ListPod::App::Lite;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+ListPod::App::Lite -
+
+=head1 SYNOPSIS
+
+  use ListPod::App::Lite;
+
+=head1 DESCRIPTION
+
+ListPod::App::Lite is
+
+=head1 AUTHOR
+
+Yusuke Wada E<lt>yusuke at kamawada.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
